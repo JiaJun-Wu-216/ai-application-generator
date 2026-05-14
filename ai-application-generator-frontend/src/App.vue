7c229777374d3e69a9d11459992df0977a0c1112
WuJiaJun <wujiajun303@foxmail.com>
2026-05-14 14:06:06 +0800

    项目前端安装 AntDesign 并将全局语言改为使用 ZH_CN

--- ai-application-generator-frontend/src/App.vue
+++ ai-application-generator-frontend/src/App.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <a-config-provider :locale="zhCN">
+    <h1>You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </a-config-provider>
 </template>
+
+<script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+
+dayjs.locale('zh-cn')
+</script>
 
 <style scoped></style>
